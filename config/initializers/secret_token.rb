48f91e2406bac48f7e4e1209ab96a57aff887147
Hoornet <hoornet@gmail.com>
2013-02-25 13:11:32 +0100

    initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoornetSampleApp::Application.config.secret_token = '93e452a6ce479f153611bb963a7f0df87d9aaeb7265319ebf36a80dbb1751a8c386d5f16ba172025618eb074e9bac78faeea0a6cd5327fffd1f5ee1d6fa1d3a1'
